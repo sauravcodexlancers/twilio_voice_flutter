290eb9fd632ddbc12e06efa80a1916aedab5582e
Nazaha <nazaha.codexlancers@gmail.com>
2024-08-14 15:59:34 +0530

    add utils file

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -2,7 +2,7 @@ package io.flutter.plugins;
 
 import io.flutter.plugin.common.PluginRegistry;
 
-import twilio.voice.flutter.codex.FlutterTwilioPlugin;
+import twilio.voice.flutter.codex.TwilioVoiceFlutterPlugin;
 
 /**
  * Generated file. Do not edit.
@@ -12,7 +12,7 @@ public final class GeneratedPluginRegistrant {
         if (alreadyRegisteredWith(registry)) {
             return;
         }
-        FlutterTwilioPlugin.registerWith(registry.registrarFor("twilio.voice.flutter.codex.FlutterTwilioPlugin"));
+        TwilioVoiceFlutterPlugin.registerWith(registry.registrarFor("twilio.voice.flutter.codex.TwilioVoiceFlutterPlugin"));
     }
 
     private static boolean alreadyRegisteredWith(PluginRegistry registry) {
---
+++ android/src/main/java/twilio/voice/flutter/codex/Utils/TwilioRegistrationListener.java
@@ -0,0 +1,7 @@
+package twilio.voice.flutter.codex.Utils;
+
+public interface TwilioRegistrationListener {
+    void onRegistered();
+
+    void onError();
+}
